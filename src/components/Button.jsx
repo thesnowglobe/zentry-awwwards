f130210608d2e39010fc4f94df9604654a4f6f8d
m0chi/ <aly0mal99@gmail.com>
2025-02-18 15:24:30 +0100

    button component jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,24 @@
-const Button = () => {
+/* eslint-disable react/prop-types */
+const Button = ({ id, title, rightIcon, leftIcon, containerClass}) => {
+
   return (
-    <div>Button</div>
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden 
+      rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
+    >
+
+      {leftIcon}
+
+      <span className='relative inline-flex overflow-hidden font-general text-xs uppercase'>
+        <div>
+          {title}
+        </div>
+      </span>
+
+      {rightIcon}
+      
+    </button>
   )
 }
 
