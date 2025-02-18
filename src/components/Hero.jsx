07b2eddfa847bb7b944e881a726bc33f55e27b77
m0chi/ <aly0mal99@gmail.com>
2025-02-18 13:51:23 +0100

    Hero component jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,17 @@
 const Hero = () => {
+  
   return (
-    <div>Hero</div>
+    <div className='relative h-dvh w-screen overflow-x-hidden'>
+      <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+        <div>
+          <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+            <div>
+              MiniVideoPlayer
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
