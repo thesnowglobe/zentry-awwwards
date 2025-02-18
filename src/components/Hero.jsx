84b7ff482ee88d019bad899b26c2e4c27107b8a7
m0chi/ <aly0mal99@gmail.com>
2025-02-18 13:57:02 +0100

    add states for Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,19 @@
+import { useRef } from "react";
+import { useState } from "react"
+
 const Hero = () => {
-  
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); 
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleMiniVideoClick = () => {
+    
+  }
+
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
