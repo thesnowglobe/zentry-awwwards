c3d186b4cf515e68bf54995df2e723f6d3b1a24b
m0chi/ <aly0mal99@gmail.com>
2025-02-18 15:42:45 +0100

    gsap animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
 import { useRef } from "react";
 import { useState } from "react"
 import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,6 +25,35 @@ const Hero = () => {
 
     setCurrentIndex(upcomingVideoIndex);
   }
+
+  useGSAP(() => {
+    if (hasClicked) {
+      gsap.set('#next-video', {visibility: 'visible'});
+
+      gsap.to('#next-video', {
+        transformOrigin: 'center center',
+        scale: 1,
+        width: '100%',
+        height: '100%',
+        duration: 1,
+        ease: 'power1.inOut',
+        onStart: () => nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video', {
+        transformOrigin: 'center center',
+        scale: 0,
+        duration: 1.5,
+        ease: 'power1.inOut'
+      })
+    }
+
+    }, 
+    {
+    dependencies: [currentIndex],
+    revertOnUpdate: true,
+    }
+  );
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -79,12 +111,18 @@ const Hero = () => {
             <Button
               id='watch-trailer'
               title='watch-trailer'
-              leftIcon={}
+              leftIcon={<TiLocationArrow />}
+              containerClass='bg-yellow-300 flex-center gap-1'
             />
 
           </div>
         </div>
       </div>
+
+      <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-black'>
+          G<b>a</b>ming
+      </h1>
+
     </div>
   )
 }
