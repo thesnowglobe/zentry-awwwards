580c08cbf4e7b1dd5943fe01730f12371bd135e2
m0chi/ <aly0mal99@gmail.com>
2025-02-18 15:59:13 +0100

    gsap animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -55,10 +55,38 @@ const Hero = () => {
     }
   );
 
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+      borderRadius: '0 0 40% 10%',
+    })
+
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0 0 0 0',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      }
+    })
+  })
+
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+      {isLoading && (
+        <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+          <div className='three-body'>
+            <div className='three-body__dot'></div>
+            <div className='three-body__dot'></div>
+            <div className='three-body__dot'></div>
+          </div>
+        </div>
+      )}
       <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div>
           <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
@@ -119,7 +147,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-black'>
+      <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
           G<b>a</b>ming
       </h1>
 
