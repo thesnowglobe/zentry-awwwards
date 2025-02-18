3cc3ea8a83d1b152d516f921c584f23ded7e4649
m0chi/ <aly0mal99@gmail.com>
2025-02-18 15:05:58 +0100

    add videos to Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useState } from "react"
+import Button from "./Button";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -31,16 +32,56 @@ const Hero = () => {
           <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
             <div onClick={handleMiniVideoClick} className='origin-center scale-50 opacity-0 transition-all duration-500 
             ease-in hover:scale-100 hover:opacity-100'>
+
               <video 
                 ref={nextVideoRef}
-                src={getVideoSrc(currentIndex + 1)} 
+                src={getVideoSrc(upcomingVideoIndex)} 
                 loop
                 muted
                 id='current-video'
                 className='size-64 origin-center scale-150 object-cover object-center'
                 onLoadedData={handleVideoLoad}
               />
+
             </div>
+          </div>
+
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id='next-video'
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video
+            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+            autoPlay
+            loop
+            muted
+            className='absolute left-0 top-0 size-full object-cover object-center'
+            onLoadedData={handleVideoLoad}
+          />
+
+        </div>
+        <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
+          G<b>a</b>ming
+        </h1>
+        <div className='absolute left-0 top-0 z-40 size-full'>
+          <div className='mt-24 px-5 sm:px-10'>
+            <h1 className='special-font hero-heading text-blue-100'>redefi<b>n</b>e</h1>
+            <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button
+              id='watch-trailer'
+              title='watch-trailer'
+              leftIcon={}
+            />
+
           </div>
         </div>
       </div>
