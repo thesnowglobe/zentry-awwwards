104f10c93b6041d6e0779557946079d9f98e4fce
m0chi/ <aly0mal99@gmail.com>
2025-02-19 12:02:56 +0100

    About section jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,30 @@ const About = () => {
   return (
     <div id='about' className='min-h-screen w-screen'>
       <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-        <h2 className='font-general text-sm uppercase md:text-[10px]'>
+        <p className='font-general text-sm uppercase md:text-[10px]'>
           Welcome to Zentry
-        </h2>
+        </p>
+
+        <div className='about-subtext'>
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className='text-gray-500'>
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
+        </div>
+      </div>
+
+      <div className='h-dvh w-screen' id='clip'>
+        <div className='mask-clip-path about-image'>
+          <img
+            src='img/about.webp'
+            alt='Background'
+            className='absolute left-0 top-0 size-full object-cover'
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default About
