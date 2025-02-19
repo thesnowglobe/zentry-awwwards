93477eac6fb7d26c88f72cc9f286ddf1478f08cd
m0chi/ <aly0mal99@gmail.com>
2025-02-19 16:12:12 +0100

    rename component + logic + gsap animations

--- src/components/FloatingImage.jsx
+++ src/components/FloatingImage.jsx
@@ -1,6 +1,49 @@
+import gsap from "gsap";
+import { useRef } from "react";
 import Button from "./Button";
+import AnimatedTitle from "./AnimatedTitle";
 
-const Story = () => {
+const FloatingImage = () => {
+  const frameRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const xPos = clientX - rect.left;
+    const yPos = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((yPos - centerY) / centerY) * -10;
+    const rotateY = ((xPos - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
+
   return (
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -9,12 +52,20 @@ const Story = () => {
         </p>
 
         <div className="relative size-full">
-          
+          <AnimatedTitle
+            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
 
           <div className="story-img-container">
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
+                  ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
                   src="/img/entrance.webp"
                   alt="entrance.webp"
                   className="object-contain"
@@ -68,7 +119,7 @@ const Story = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Story
+export default FloatingImage;
